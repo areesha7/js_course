1d0edcb85d90624a36333d831ec6a272e929c994
areesha7 <121893993+areesha7@users.noreply.github.com>
2025-01-21 06:59:59 +0000

    while loop

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,25 @@
+// const isLoggedIn= true
+// while (isLoggedIn==true) {
+//     console.log(`user is logged In`)
+//     break
+// }
+
+// let index=0
+// while (index<=10) {
+//     console.log(`${index}`)
+//     index= index+ 2
+// }
+
+// let arr= ['a', 'b','c','d','e']
+// let i=0;
+// while (i<=arr.length) {
+//     console.log(`${arr[i]}`)
+//     i+=1
+// }
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score<=10);
